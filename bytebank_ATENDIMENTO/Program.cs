be7ff3b43a392fdc9ed716dcf125c52e51face9c
SAMUEL MOTA <samuelhpmota@gmail.com>
2024-12-17 08:56:31 -0300

    Aula 2 -part 4

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,39 +1,60 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Util;
 
-TestaArray();
-void TestaArray()
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+//TestaArray();
+//void TestaArray()
+//{
+//    int[] idade = { 30, 40, 17, 21, 18 };
+//    Console.WriteLine($"O tamanho do array de idade é: {idade.Length}");
+
+//    int total =0;
+//    for (int i = 0; i < idade.Length; i++)
+//    {
+//        total += idade[i];
+//    }
+//    double media = total / idade.Length;
+//    Console.WriteLine($"A média das idades é: {media}");
+//}
+////BuscaPalavras();
+//void BuscaPalavras()
+//{
+//    string[] arrayDePalavras = new string[5];
+
+//    for (int i = 0; i < arrayDePalavras.Length; i++)
+//    {
+//        Console.WriteLine($"Digite a palavra da posicao: {i+1}");
+//        arrayDePalavras[i] = Console.ReadLine()!;
+//    }
+
+//    Console.WriteLine("Agora vamos encontrar uma palavra");
+//    Console.Write("Digite a palavra que deseja encontrar:");
+//    string palavra = Console.ReadLine()!;
+
+//    for (int i = 0; i < arrayDePalavras.Length; i++) 
+//    {
+//        if(arrayDePalavras[i].Equals(palavra))
+//        {
+//            Console.WriteLine($"A palavra está na posicao: {i+1}");
+//        };
+//    }
+//}
+
+//Array amostra = Array.CreateInstance(typeof(double), 5);
+//amostra.SetValue(5.9, 0);
+//amostra.SetValue(1.8, 1);
+//amostra.SetValue(7.1, 2);
+//amostra.SetValue(10, 3);
+//amostra.SetValue(6.9, 4);
+
+TestaArrayDeContas();
+void TestaArrayDeContas()
 {
-    int[] idade = { 30, 40, 17, 21, 18 };
-    Console.WriteLine($"O tamanho do array de idade é: {idade.Length}");
+    ListaDeContaCorrentes listaDeContaCorrentes = new ListaDeContaCorrentes();
+    listaDeContaCorrentes.Adicionar(new ContaCorrente(874));
+    listaDeContaCorrentes.Adicionar(new ContaCorrente(875));
+    listaDeContaCorrentes.Adicionar(new ContaCorrente(876));
+
     
-    int total =0;
-    for (int i = 0; i < idade.Length; i++)
-    {
-        total += idade[i];
-    }
-    double media = total / idade.Length;
-    Console.WriteLine($"A média das idades é: {media}");
-}
-BuscaPalavras();
-void BuscaPalavras()
-{
-    string[] arrayDePalavras = new string[5];
-
-    for (int i = 0; i < arrayDePalavras.Length; i++)
-    {
-        Console.WriteLine($"Digite a palavra da posicao: {i+1}");
-        arrayDePalavras[i] = Console.ReadLine()!;
-    }
-
-    Console.WriteLine("Agora vamos encontrar uma palavra");
-    Console.Write("Digite a palavra que deseja encontrar:");
-    string palavra = Console.ReadLine()!;
-
-    for (int i = 0; i < arrayDePalavras.Length; i++) 
-    {
-        if(arrayDePalavras[i].Equals(palavra))
-        {
-            Console.WriteLine($"A palavra está na posicao: {i+1}");
-        };
-    }
 }
---
+++ bytebank_ATENDIMENTO/bytebank.Util/ListaDeContaCorrentes.cs
@@ -0,0 +1,34 @@
+﻿using bytebank.Modelos.Conta;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util;
+
+public class ListaDeContaCorrentes
+{
+    private ContaCorrente[] _itens = null;
+    private int _proximaPosicao = 0;
+
+    public ListaDeContaCorrentes(int tamanhoInicial = 5)
+    {
+        _itens = new ContaCorrente[tamanhoInicial];
+    }
+
+    public void Adicionar(ContaCorrente conta)
+    {
+        VerificarCapacidade(_proximaPosicao + 1);
+        _itens[_proximaPosicao] = conta;
+        Console.WriteLine($"Item adicionado na posicao {_proximaPosicao}");
+        _proximaPosicao++;
+    }
+
+    private void VerificarCapacidade(int tamanhoNecessario)
+    {
+        if (_itens.Length >= tamanhoNecessario)
+        {
+            return;
+        }
+        Console.WriteLine("Aumentando tamanho da lista....");
+        ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+        Array.Copy(_itens, novoArray, _itens.Length);
+        _itens = novoArray;
+    }
+}
